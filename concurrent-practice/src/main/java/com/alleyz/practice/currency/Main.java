909b2f8226187c56146d92c0e92d0f8615668507
alleyz <alleyz@126.com>
2018-01-17 15:15:16 +0800

    init

---
+++ concurrent-practice/src/main/java/com/alleyz/practice/currency/Main.java
@@ -0,0 +1,17 @@
+package com.alleyz.practice.currency;
+
+/**
+ * date: 2018-01-17
+ * author: alleyz
+ * email: alleyz@126.com
+ */
+public class Main {
+
+    public void sayHello(){
+        System.out.println(" say ");
+    }
+
+    public String get() {
+        return 1 + "";
+    }
+}
